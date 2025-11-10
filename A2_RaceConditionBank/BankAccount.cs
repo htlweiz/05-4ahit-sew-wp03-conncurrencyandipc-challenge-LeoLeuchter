37b0a34483cda835d3b4e95e4c252d14cd0db709
Leonard Reisner <leonard.reisner@gmail.com>
2025-11-10 08:53:44 +0100

    new better super

--- A2_RaceConditionBank/BankAccount.cs
+++ A2_RaceConditionBank/BankAccount.cs
@@ -4,25 +4,32 @@ using System.Threading;
 namespace A2_RaceConditionBank;
 public class BankAccount
 {
+    static object lockobject = new object();
     private int balance;
-   
-    
-    public BankAccount(int initial) 
-    { 
-        balance = initial; 
+
+
+    public BankAccount(int initial)
+    {
+        balance = initial;
     }
-    
-    public void Deposit(int amount) 
-    { 
-       
+
+    public void Deposit(int amount)
+    {
+        lock (lockobject)
+        {
+            balance = +amount;
+        }
     }
-    
-    public void Withdraw(int amount) 
-    { 
-        
+
+    public void Withdraw(int amount)
+    {
+        lock (lockobject)
+        {
+            balance = -amount;
+        }
     }
-    
-    public int GetBalance() 
+
+    public int GetBalance()
     {
         return balance;
     }
--- A2_RaceConditionBank/Program.cs
+++ A2_RaceConditionBank/Program.cs
@@ -9,16 +9,33 @@ class Program
     {
         Console.WriteLine("Übung 2: Race Condition – Bankkonto");
         Console.WriteLine("==========================================\n");
-        
+
         // Bankkonto mit Startwert 1000 EUR erstellen
         BankAccount account = new BankAccount(1000);
         Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
-        
+
+        List<Thread> lthreads = new List<Thread>();
+
+        for (int count = 0; count < 10; count++)
+        {
+            Thread thread = new Thread(() => PerformBankOperations(account));
+            lthreads.Add(thread);
+            thread.Start();
+        }
+
+        foreach (Thread t in lthreads)
+        {
+            t.Join();
+            Console.WriteLine("new Thread");
+        }
     }
     
     private static void PerformBankOperations(BankAccount account)
     {
-        
+        BankAccount accountt = new BankAccount(1000);
+        accountt.Deposit(100);
+        Thread.Sleep(100);
+        accountt.Withdraw(150);
     }
 }
 
